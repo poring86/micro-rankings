ddd8b877a83b313c3ae7958767cee36f2f670dd3
Matheus Lino <matheuslino86a@gmail.com>
2022-09-19 23:19:41 -0400

    Timezone

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
+import * as momentTimezone from 'moment-timezone';
 
 const configService = new ConfigService();
 
@@ -18,6 +19,12 @@ async function bootstrap() {
       queue: 'rankings',
     },
   });
+
+  Date.prototype.toJSON = function () {
+    return momentTimezone(this)
+      .tz('America/Sao_Paulo')
+      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  };
 
   await app.listen();
 }
